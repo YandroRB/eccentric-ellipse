011328330b38c551474014ce4ae8ccb38f9a48a7
YandroRB <santiago.rdbn@outlook.com>
2025-08-05 19:51:43 -0500

    Agregacion del adaptador

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   integrations:[react()],
-  output:'server'
+  output:'server',
+  adapter:vercel(),
 });
